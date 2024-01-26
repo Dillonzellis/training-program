21e24f5862c2bd58bfa227d4647b983071b37e80
dillonzellis <dillonzellis@gmail.com>
2024-01-26 16:56:48 -0500

    feat: add breadcrumb and set data

---
+++ src/data/crumbs.ts
@@ -0,0 +1,38 @@
+export interface CrumbType {
+  name: string;
+  href: string;
+}
+
+export const PPLCrumbs = [
+  {
+    name: "Push",
+    href: "/",
+  },
+  {
+    name: "Pull",
+    href: "/",
+  },
+  {
+    name: "Legs",
+    href: "/",
+  },
+];
+
+export const FourDayCrumbs = [
+  {
+    name: "Chest",
+    href: "/",
+  },
+  {
+    name: "Chest",
+    href: "/",
+  },
+  {
+    name: "Chest",
+    href: "/",
+  },
+  {
+    name: "Chest",
+    href: "/",
+  },
+];
---
+++ src/data/sets.ts
@@ -0,0 +1,22 @@
+export const chest = [
+  "Incline Bench Press",
+  "Flat Bench Press",
+  "Cable Crossovers/Flys",
+] as const;
+
+export const triceps = ["Cable Pushdown", "Skull Crushers", "Dips"] as const;
+
+export const shoulders = [
+  "Shoulder Press",
+  "Cable Lateral Raise",
+  "Cable Face Pull",
+  "Cable Top Down",
+] as const;
+
+export const back = [
+  "Lat Pulldown",
+  "Mid Row",
+  "High Row",
+  "Pull Over",
+  "Single Arm Pull Over",
+] as const;
