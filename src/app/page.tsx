2c10c3b1f9bc4d80d75bd6799148b128b6446bae
dillonzellis <dillonzellis@gmail.com>
2024-01-26 14:10:19 -0500

    feat: basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
+      <div>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pb-6">
+          Training Program
+        </h1>
+        <p className="text-sm text-muted-foreground pb-16">1/26/24</p>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight">
+          Splits:
+        </h2>
+
+        <div className="flex flex-col justify-start items-start">
+          <Button asChild className="p-0" variant="link">
+            <Link href="/">Push/Pull/Legs</Link>
+          </Button>
+
+          <Button asChild className="p-0" variant="link">
+            <Link href="/">Chest/Back/Arms/Legs</Link>
+          </Button>
+        </div>
+      </div>
     </main>
   );
 }
