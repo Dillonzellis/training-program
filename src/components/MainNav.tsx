347da44602bccbad1e38dfa618e6b14376b6028b
dillonzellis <dillonzellis@gmail.com>
2024-01-26 18:11:44 -0500

    style: change MainNav style and split links

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -5,14 +5,19 @@ export default function MainNav() {
     <nav>
       <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
         <div className="flex items-center justify-between gap-4 border-b pb-2 pt-3 text-sm font-normal">
-          <div className="flex items-center">
-            <Link
-              className="pr-6 font-serif text-xl lg:pr-12 lg:text-2xl"
-              href="/"
-            >
+          <div className="flex items-center gap-8">
+            <Link className="pr-6 text-xl lg:pr-24 lg:text-2xl" href="/">
               Training Program
             </Link>
-            <Link href="/ppl">Push/Pull/Legs</Link>
+            <Link href="/ppl" className="hover:text-muted-foreground">
+              Push/Pull/Legs
+            </Link>
+            <Link
+              href="/four-day-split"
+              className="hover:text-muted-foreground"
+            >
+              4 Day Split
+            </Link>
           </div>
         </div>
       </div>
