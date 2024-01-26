10cdefae4605df63e32d6ce2c961cb7b80808a16
dillonzellis <dillonzellis@gmail.com>
2024-01-26 17:42:02 -0500

    style: change padding on MainWrapper

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -4,7 +4,7 @@ interface MainWrapperProps {
 
 export const MainWrapper = ({ children }: MainWrapperProps) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:py-24 py-8">
       {children}
     </main>
   );
