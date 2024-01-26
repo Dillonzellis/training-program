9a8ddc4453eb337535f2f370e724a9cca056e1c7
dillonzellis <dillonzellis@gmail.com>
2024-01-26 18:07:17 -0500

    feat: add ppl leg day page

---
+++ src/app/ppl/legs/page.tsx
@@ -0,0 +1,32 @@
+import { BreadCrumb } from "@/components/BreadCrumb";
+import { MainWrapper } from "@/components/MainWrapper";
+import { H1 } from "@/components/typography/H1";
+import { H2 } from "@/components/typography/H2";
+import { PPLCrumbs } from "@/data/crumbs";
+import { legs } from "@/data/sets";
+
+export default function PPLLegs() {
+  return (
+    <MainWrapper>
+      <div>
+        <H1 className="pb-8">Legs</H1>
+
+        <BreadCrumb crumbs={PPLCrumbs} />
+
+        <H2>Warmup</H2>
+
+        <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+          <li>3 Sets of Sleigh Push/Pulls</li>
+        </ul>
+
+        <H2>Hamstring/Quads/Calfs/Accessories</H2>
+
+        <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+          {legs.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </MainWrapper>
+  );
+}
