72d72916e486ad4e831abf3b62c78431297ebf67
dillonzellis <dillonzellis@gmail.com>
2024-08-03 12:33:27 -0400

    update splits names data

--- src/data/splits.ts
+++ src/data/splits.ts
@@ -1,4 +1,5 @@
 export const splits = [
+  { name: "4 Day Split V2", href: "/4-day-split-v2" },
   { name: "5 Day Split", href: "/5-day-split" },
   { name: "4 Day Split", href: "/4-day-split" },
   { name: "Push/Pull/Legs", href: "/ppl" },
