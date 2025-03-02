8c1626b1e6baa63cffd8c12c11f748b0f018be0f
dillonzellis <dillonzellis@gmail.com>
2025-03-02 12:08:32 -0500

    update update date

--- src/data/globals.ts
+++ src/data/globals.ts
@@ -1,2 +1,3 @@
 // export const updateDate = "8/05/2024"; // 4 Day split v2
-export const updateDate = "1/05/2025"; // PPL Split
+// export const updateDate = "1/05/2025"; // PPL Split
+export const updateDate = "3/02/2025"; // PPL Split updates
